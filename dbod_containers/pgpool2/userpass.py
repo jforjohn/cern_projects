0aeca7a9f91231288b09086abd8fb81ef487c3ca
jforjohn <giannisotiro@gmail.com>
2016-10-10 17:34:10 +0200

    dbod:containers:pgpool example file with user's credentials to access the database

---
+++ dbod_containers/pgpool2/userpass.py
@@ -0,0 +1,16 @@
+userpassDict = {
+'pcp_port': 9898,
+'pcp_user': 'pcpuser',
+'pcp_password': 'mypass',
+'pool_port': 5432,
+'pool_user': 'admin',
+'pool_password': 'dbod-postgres-test',
+'recovery_user': 'duper',
+'recovery_password': 'superduper',
+'sr_check_user': 'checker',
+'sr_check_password': 'test',
+'sr_check_database': 'checker',
+'health_check_user': 'checker',
+'health_check_password': 'test',
+'health_check_database': 'checker',
+}
