4e8bae4e20bde170490416e992bfbf4fbf795457
jforjohn <giannisotiro@gmail.com>
2016-10-10 17:30:19 +0200

    dbod:containers:pgpool/postgres direct access to postgres user

--- dbod_containers/pgpool2/pgbin/pgpool_remote_start.sh
+++ dbod_containers/pgpool2/pgbin/pgpool_remote_start.sh
@@ -2,10 +2,11 @@
 
 HOST_TO_RECOVER=$1
 HOST_TO_RECOVER_DB_CLUSTER=$2
-HOST_TO_RECOVER_USER=dbod
+HOST_TO_RECOVER_USER=postgres
 
-PG_CTL=/usr/pgsql-$PG_VERSION_MAJOR/bin/pg_ctl
-LOG=/var/lib/pgsql/pgdata/recovery.log
+PG_VERSION=9.4.5
+PG_CTL=/usr/local/pgsql/pgsql-$PG_VERSION_MAJOR/bin/pg_ctl
+LOG=recovery.log
 
 # remote start
 echo -e "$(date) - $(basename $0)\n \
@@ -13,6 +14,6 @@ echo -e "$(date) - $(basename $0)\n \
     >> $LOG
 
 echo "Starting remotely the $HOST_TO_RECOVER" >> $LOG
-ssh -t $HOST_TO_RECOVER_USER@$HOST_TO_RECOVER "sudo -iu postgres $PG_CTL -w -D $HOST_TO_RECOVER_DB_CLUSTER start" >> $LOG
+ssh -t $HOST_TO_RECOVER_USER@$HOST_TO_RECOVER "$PG_CTL -w -l $HOST_TO_RECOVER_DB_CLUSTER/postgresql.log -D $HOST_TO_RECOVER_DB_CLUSTER start" >> $LOG 2>&1
 
 exit 0;
